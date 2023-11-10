f0837821ae8d1ac2f5d2486d5606547c2da02168
Alexander <alexandercryptock@icloud.com>
2023-11-09 19:05:38 -0500

    app working

--- server.js
+++ server.js
@@ -2,12 +2,34 @@ const express = require('express')
 const fs = require('fs')
 const path = require('path')
 const data = require('./notes.json')
-const { uuid } = require('uuidv4');
+const { v4: uuidv4 } = require('uuid')
+const util = require('util')
+
+const readAndAppend = (content, file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+const readFromFile = util.promisify(fs.readFile);
 
 const PORT = 8080;
 const server = express();
 
+// making public folder static for users
 server.use(express.static('public'));
+
+// middleware
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 
@@ -15,82 +37,38 @@ server.use(express.json());
 server.get('/', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
-
 // get route for notes
 server.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 // get route for invalid route to 404 page
+
+server.route('/api/notes')
+.get(function (req, res) {
+  readFromFile('notes.json').then((data) => res.json(JSON.parse(data)));
+})
+
+.post(function (req, res) {
+  console.log(req.body);
+  
+  const { title, text } = req.body;
+  
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuidv4(),
+    };
+    
+    readAndAppend(newNote, './notes.json');
+    res.json(`Note added successfully`);
+  } else {
+    res.error('Error in adding Note');
+  }
+});
 server.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/404.html'))
 );
-
-server.route('/api/notes')
-    .get(function (req, res) {
-        res.json(data)
-    })
-
-    .post(function (req, res) {
-        console.log(req.body);
-      
-        const { title, text } = req.body;
-      
-        if (req.body) {
-          const newNote = {
-            title,
-            text,
-            note_id: uuidv4(),
-          };
-      
-          readAndAppend(newNote, '/notes.json');
-          res.json(`Note added successfully`);
-        } else {
-          res.error('Error in adding Note');
-        }
-      });
-
-    // .post(function (req, res) {
-    //     var json = path.join(__dirname, './notes.json');
-    //     var myNote = req.body;
-
-    //     // This allows the test note to be the original note.
-    //     var highestId = 99;
-    //     // This loops through the array and finds the highest ID.
-    //     for (var i = 0; i < database.length; i++) {
-    //         var individualNote = database[i];
-
-    //         if (individualNote.id > highestId) {
-    //             // highestId will always be the highest numbered id in the notesArray.
-    //             highestId = individualNote.id;
-    //         }
-    //     }
-    //     // This assigns an ID to the myNote. 
-    //     myNote.id = highestId + 1;
-    //     // We push it to db.json.
-    //     database.push(myNote)
-
-    //     // Write the db.json file again.
-    //     fs.writeFile(json, JSON.stringify(database), function (err) {
-
-    //         if (err) {
-    //             return console.log(err);
-    //         }
-    //         console.log("Your note was saved!");
-    //     });
-    //     // Gives back the response, which is the user's new note. 
-    //     res.json(myNote);
-    // });
-
-
-
-
-
-
-
-
-
-
-
 
 // launches server
 server.listen(PORT, () =>
